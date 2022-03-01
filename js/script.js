94c1b0366997083bb6bc446a8257839032e03c28
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2022-03-01 20:57:08 -0300

    caminho da animaisapi corrigido

--- js/script.js
+++ js/script.js
@@ -44,6 +44,6 @@ menuMobile.init()
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init()
 
-fetchAnimais('../../animais-api.json', '.numeros-grid')
+fetchAnimais('./animais-api.json', '.numeros-grid')
 
 FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
